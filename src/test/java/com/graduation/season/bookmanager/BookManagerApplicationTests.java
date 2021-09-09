f30d84c9435869d03f0700ab0fa2e2281dc09920
tianyafu <futy@huawangtech.com>
2021-09-09 11:44:16 +0800

    init project

---
+++ src/test/java/com/graduation/season/bookmanager/BookManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.graduation.season.bookmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
